14906f015eece41b3d0b59d4797eb9c818d74eb7
carolinesales <caroline.vsales@gmail.com>
2025-05-20 21:01:09 -0300

    refatoração: organização da estrutura em uma pasta única e inclusão de mais dados em ReservaAreaComum

---
+++ src/CadastroInquilino.java
@@ -0,0 +1,40 @@
+package src;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CadastroInquilino {
+    private List<Inquilino> inquilinos;
+
+    public CadastroInquilino() {
+        inquilinos = new ArrayList<>();
+    }
+
+    public void cadastrarInquilino() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do inquilino: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite o número do apartamento: ");
+        String apartamento = scanner.nextLine();
+
+        System.out.print("Digite o telefone do inquilino: ");
+        String telefone = scanner.nextLine();
+
+        Inquilino inquilino = new Inquilino(nome, apartamento, telefone);
+        inquilinos.add(inquilino);
+
+        System.out.println("Inquilino cadastrado com sucesso!");
+    }
+
+    public void exibirInquilinos() {
+        if (inquilinos.isEmpty()) {
+            System.out.println("Nenhum inquilino cadastrado.");
+        } else {
+            for (Inquilino inquilino : inquilinos) {
+                inquilino.exibirInformacoes();
+            }
+        }
+    }
+}
---
+++ src/ReservaAreaComum.java
@@ -0,0 +1,56 @@
+package src;
+
+public class ReservaAreaComum {
+    private Inquilino inquilino;
+    private String dataReserva;  
+    private String horaReserva; 
+    private String areaComum;
+
+    public ReservaAreaComum(String nome, String apartamento, String telefone,
+                            String dataReserva, String horaReserva, String areaComum) {
+        this.inquilino = new Inquilino(nome, apartamento, telefone);
+        this.dataReserva = dataReserva;
+        this.horaReserva = horaReserva;
+        this.areaComum = areaComum;
+    }
+
+
+    public Inquilino getInquilino() {
+        return inquilino;
+    }
+
+    public String getDataReserva() {
+        return dataReserva;
+    }
+
+    public String getHoraReserva() {
+        return horaReserva;
+    }
+
+    public String getAreaComum() {
+        return areaComum;
+    }
+
+    // Setters
+    public void setDataReserva(String dataReserva) {
+        this.dataReserva = dataReserva;
+    }
+
+    public void setHoraReserva(String horaReserva) {
+        this.horaReserva = horaReserva;
+    }
+
+    public void setAreaComum(String areaComum) {
+        this.areaComum = areaComum;
+    }
+
+   
+    public void exibirInformacoes() {
+        System.out.println("Reserva de Área Comum:");
+        System.out.println("Área: " + areaComum);
+        System.out.println("Data: " + dataReserva);
+        System.out.println("Hora: " + horaReserva);
+        System.out.println("Inquilino:");
+        inquilino.exibirInformacoes();
+    }
+}
